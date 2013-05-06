2a4935c861dac86f2d42d3a0993883c545594f1e
fuksman <sergey@fuksman.com>
2013-05-07 02:13:13 +0400

    Удален the_roles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,3 @@
 class ApplicationController < ActionController::Base
-  #include TheRole::Requires
-
   protect_from_forgery
-
-  #def access_denied
-  #  render :text => 'access_denied: requires an role' and return
-  #end
-
-  #alias_method :login_required,     :authenticate_user!
-  #alias_method :role_access_denied, :access_denied
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130506171424) do
+ActiveRecord::Schema.define(:version => 20130506215132) do
 
   create_table "bases", :force => true do |t|
     t.string  "name"
@@ -103,7 +103,6 @@ ActiveRecord::Schema.define(:version => 20130506171424) do
     t.string   "apartments"
     t.string   "study_type"
     t.boolean  "have_debts"
-    t.integer  "role_id"
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "social_activity"
